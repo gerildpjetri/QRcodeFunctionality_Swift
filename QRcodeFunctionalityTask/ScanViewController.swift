29ecdd42646e7f4213e0e1f05673e37a745981ba
gerildpjetri <gerild.pjetri@outlook.com>
2021-08-11 12:04:47 +0200

    Gerild - Improve QR code functionality

--- QRcodeFunctionalityTask/ScanViewController.swift
+++ QRcodeFunctionalityTask/ScanViewController.swift
@@ -22,6 +22,7 @@ protocol ScanProtocol {
 class ScanViewController: UIViewController , AVCaptureMetadataOutputObjectsDelegate {
 
     
+    @IBOutlet var focusScanView: UIView!
     
     var containerViewDelegate: ScanProtocol?
     
@@ -34,8 +35,125 @@ class ScanViewController: UIViewController , AVCaptureMetadataOutputObjectsDeleg
         super.viewDidLoad()
 
         
+        
+        captureSession = AVCaptureSession()
+
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if (captureSession.canAddInput(videoInput)) {
+            
+            captureSession.addInput(videoInput)
+            
+        } else {
+            failed()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr,
+                                                  .code128,
+                                                  .code39,
+                                                  .code39Mod43,
+                                                  .code93,
+                                                  .ean8,
+                                                  .interleaved2of5,
+                                                  .itf14,
+                                                  .pdf417,
+                                                  .upce ]
+            
+        } else {
+            failed()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = focusScanView.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        focusScanView.layer.addSublayer(previewLayer)
+        //focusScanView.bringSubviewToFront(logoImageInCodeReader)
+        captureSession.startRunning()
+        
+        
+        
+        
+        
+        
+        
     }
     
+    
+    
+    
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+    
+
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        
+        
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+        captureSession.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+
+        //dismiss(animated: true)
+    }
+
+    func found(code: String) {
+        
+        print(code)
+        containerViewDelegate?.showResult(result: code)
+        
+        
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
 
     
 
--- QRcodeFunctionalityTask/ViewController.swift
+++ QRcodeFunctionalityTask/ViewController.swift
@@ -7,23 +7,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , ScanProtocol{
+    
+    
 
+    
     
     
     @IBOutlet var chooseSearchType: UISegmentedControl!
     @IBOutlet var containerViewScan: UIView!
     
+    var childQRKodVC : ScanViewController!
     
+    @IBOutlet var customerNumberLabel: UILabel!
+    @IBOutlet var enterCustomerNumberInputField: UITextField!
     
+    var scanActivated: Bool = false
+    
+    @IBOutlet var contentQRCode: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        contentQRCode.isHidden = true
+        
+        childQRKodVC = self.storyboard?.instantiateViewController(withIdentifier: "ScanViewController") as? ScanViewController
+        childQRKodVC.containerViewDelegate = self
         
         
     }
+    
+    
+    @IBAction func choosesearch(_ sender: Any) {
+        
+        
+        if chooseSearchType.selectedSegmentIndex == 0 {
+            
+            contentQRCode.isHidden = true
+            customerNumberLabel.isHidden = false
+            enterCustomerNumberInputField.isHidden = false
+            containerViewScan.isHidden = true
+            
+            
+            if (scanActivated){
+                
+             childQRKodVC.willMove(toParent: nil)
+             childQRKodVC.view.removeFromSuperview()
+             childQRKodVC.removeFromParent()
+             scanActivated = false
+                
+            }
+            
+            
+            
+        }
+        else if chooseSearchType.selectedSegmentIndex == 1{
+            
+            customerNumberLabel.isHidden = true
+            enterCustomerNumberInputField.isHidden = true
+            containerViewScan.isHidden = false
+            scanActivated = true
+            
+            addChild(childQRKodVC)
+            //Or, you could add auto layout constraint instead of relying on AutoResizing contraints
+            childQRKodVC.view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+            childQRKodVC.view.frame = containerViewScan.bounds
+            
+            containerViewScan.addSubview(childQRKodVC.view)
+            childQRKodVC.didMove(toParent: self)
+            
+        }
+        
+        
+        
+        
+        
+    }
+    
+    func showResult(result: String) {
+        
+        
+        contentQRCode.text = result
+        contentQRCode.isHidden = false
+        
+    }
 
+    
+    
 
 }
 
